e95818a658fadc05a0eb5f3c56b05b7a2a14600b
[Connor Boscherini] <connorbos@hotmail.co.uk>
2024-05-02 14:54:29 +0100

    US1-T1: Test written and passed successfully

--- spec/account.spec.js
+++ spec/account.spec.js
@@ -1,20 +1,22 @@
-import account from "../src/account.js";
+import Account from "../src/Account.js";
+
+let testAccount, expected;
 
 describe("Account Tests:", () => {
 
     beforeEach(() => {
-            
+        testAccount = new Account();
     });
 
     afterEach(() => {
-            
+        testAccount = undefined;
         expected = undefined;
     });
 
     describe("User Story 1 Tests:", () => {
-        it("Test1: ", () => {
+        it("Test1: Users account is an instance of the Account class", () => {
             
-            
+            expect(testAccount instanceof (Account)).toBeTrue();
         });
     });
 
