4b03580a0707b9c1811390ad2a232a740dee85c7
[Connor Boscherini] <connorbos@hotmail.co.uk>
2024-05-02 14:44:36 +0100

    NEW: Set up starting environment

---
+++ spec/account.spec.js
@@ -0,0 +1,22 @@
+import account from "../src/account.js";
+
+describe("Account Tests:", () => {
+
+    beforeEach(() => {
+            
+    });
+
+    afterEach(() => {
+            
+        expected = undefined;
+    });
+
+    describe("User Story 1 Tests:", () => {
+        it("Test1: ", () => {
+            
+            
+        });
+    });
+
+});
+    
